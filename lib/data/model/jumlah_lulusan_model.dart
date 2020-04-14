8a25560b961255a08cc63cb993c06aeac3972353
fadilmuh22 <fadilmuh2002@gmail.com>
2020-04-14 19:22:02 +0700

    fix the third jumlah lulusan label

--- lib/data/model/jumlah_lulusan_model.dart
+++ lib/data/model/jumlah_lulusan_model.dart
@@ -1,3 +1,5 @@
+import 'package:isk_aps_calc/constants.dart';
+
 class JumlahLulusanModel {
   String type, variable;
   List<String> title;
@@ -6,13 +8,23 @@ class JumlahLulusanModel {
   JumlahLulusanModel({
     this.type,
     this.variable,
-    this.title = const [
-      'Jumlah Lulusan TS-4',
-      'Jumlah Lulusan TS-3',
-      'Jumlah Lulusan TS-2'
-    ],
+    this.title,
     this.value = const [0, 0, 0],
-  });
+  }) {
+    if (this.type == Constants.lulusanTanggap) {
+      this.title = [
+        'Jumlah Koresponden TS-4',
+        'Jumlah Koresponden TS-3',
+        'Jumlah Koresponden TS-2'
+      ];
+    } else {
+      this.title = [
+        'Jumlah Lulusan TS-4',
+        'Jumlah Lulusan TS-3',
+        'Jumlah Lulusan TS-2'
+      ];
+    }
+  }
 
   JumlahLulusanModel.fromJson(Map<String, dynamic> json) {
     type = json['type'];
