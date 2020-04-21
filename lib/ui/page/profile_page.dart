de8d67128387639e3aaaf72405dd0d0c5c8d3145
maulana <maulana@neuronworks.co.id>
2020-04-22 01:27:54 +0700

    run build_runner, remove unused lib

--- lib/ui/page/profile_page.dart
+++ lib/ui/page/profile_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 import 'package:image_picker/image_picker.dart';
 
@@ -267,7 +266,6 @@ class _ProfilePageState extends State<ProfilePage> {
               ],
             ),
           ),
-          // SizedBox(height: 20.0),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
