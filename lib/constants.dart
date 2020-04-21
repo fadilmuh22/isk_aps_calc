530a6c318dfe8777ee6aab0a74301e99526c354b
maulana <maulana@neuronworks.co.id>
2020-04-22 01:24:44 +0700

    update stylte text guide

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Constants {
   static const titleStyle =
-      TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+      TextStyle(fontSize: 10, fontWeight: FontWeight.bold);
 
   static const Color primaryColor = Color(0xffFFFFFF); //Colors.redAccent
   static const Color accentColor = Color(0xffC82247);
