9c1c612217bc4f1ec8a147136cfbabd36fc473ae
maulana <maulana@neuronworks.co.id>
2020-05-09 20:36:54 +0700

    scrollable drafter bottom sheet

--- lib/ui/page/simulation/result_page.dart
+++ lib/ui/page/simulation/result_page.dart
@@ -128,6 +128,7 @@ class _ResultPageState extends State<ResultPage> {
 
   void showDrafter(String description, String title) {
     showModalBottomSheet(
+        isScrollControlled: true,
         context: context,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.only(
@@ -138,7 +139,7 @@ class _ResultPageState extends State<ResultPage> {
         backgroundColor: Colors.white,
         elevation: 16.0,
         builder: (BuildContext bc) {
-          return Container(
+          return SingleChildScrollView(
             padding: EdgeInsets.symmetric(vertical: 48.0, horizontal: 20.0),
             child: Wrap(
               children: <Widget>[
